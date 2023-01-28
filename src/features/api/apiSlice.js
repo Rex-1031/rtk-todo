067ef4ef2d0c23edf07282a60ad483380ae5ad07
Steve Clair <sclair@bird-technologies.com>
2023-01-27 23:23:02 -0500

    wip

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const apiSlice = createApi({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500'}),
+    endpoints: (builder)=>({
+        getTodos: builder.query({
+            query: ()=> '/todos',
+        }),
+        addTodo: builder.mutation({
+            query: (todo)=>({
+                url: '/todos',
+                method: 'POST',
+                body: todo
+            })
+        }),
+        updateTodo: builder.mutation({
+            query: (todo)=>({
+                url: `/todos/${todo.id}`,
+                method: 'PATCH',
+                body: todo
+            })
+        })
+    })
+})
+
+export const {
+    useGetTodosQuery
+} = apiSlice
